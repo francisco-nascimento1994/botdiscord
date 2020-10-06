876720489688d6f50e92bcb3f99dbade3c9ff4ac
semskillcfg <68784263+semskillcfg@users.noreply.github.com>
2020-10-06 22:45:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     timezone: 'Europe/Lisbon', // Timezone (take a look at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List)
     format: 'HH:mm (z)', // Clock format, leave this default seting for 24h format, read more at https://momentjs.com/docs/#/displaying/format/
     updateinterval: 60000, // Discord is ratelimiting us for 10 minutes!
-    //[ON WORK, IGNORE THIS FIELD] dev: '400581909912223744', // Developer's ID for sending the errors
+    
   }
