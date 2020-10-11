4463d910d05a5ef86121934e67cafa5827232112
Francisco <68784263+francisco-nascimento1994@users.noreply.github.com>
2020-10-11 19:19:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //all fields are **required**
-    token2: 'NzM2NTQxMTQyNjY1MDAzMDIx.XxwTZA.24ESIdQlpAiMfCjCqWAEaxGKmUw', // Your bot's token
+    token2: 'deleted', // Your bot's token
     clockchannel: '763070617721241610', // ID of a voice channel that used to display the time
     timezone: 'Europe/Lisbon', // Timezone (take a look at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List)
     format: 'HH:mm (z)', // Clock format, leave this default seting for 24h format, read more at https://momentjs.com/docs/#/displaying/format/
